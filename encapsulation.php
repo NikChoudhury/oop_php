7cc409884b241b9cd0c56c5334b31c7119781f76
Nikumani Choudhury <nikunchoudhury@gmail.com>
2021-07-13 14:35:46 +0530

    Encapsulation

--- encapsulation.php
+++ encapsulation.php
@@ -6,11 +6,54 @@
 */
 
 
+class Class1{
+	protected $num;
+	function __construct(){
+		$this->num=1;
+	}
+
+	function getNum(){
+		return $this->num;
+	}
+}
+
+$obj1= new Class1;
+
+// $obj1->num=2;
+
+// echo $obj1->num;
+
+// echo $obj1->getNum();
 
 
+////////////////////////////////////////////
 
 
+class Class2
+{
+	protected $num;
+	function __construct(){
+		$this->num=1;
+	}
+	
+}
+
+class Class3 extends Class2
+{
+	function getNum(){
+		return $this->num;
+	}
+	
+}
+
+$obj3= new Class3;
+echo $obj3->getNum();
 
 
+/*
+public -> Access Everywhere
+protected -> Access Inside The Class and Extended Class( Inheritance)
+private -> Access Within The Class
+*/
 
 ?>
